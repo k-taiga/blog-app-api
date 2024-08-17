7c62fc6333d2c3b96ac2bd6d441bc54c1e742b16
K-taiga <taigakatou0329@gmail.com>
2024-08-17 18:03:34 +0900

    [add] repositories

---
+++ src/repositories/articles.go
@@ -0,0 +1,73 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/k-taiga/blog-app-api/models"
+)
+
+const (
+	articleNumPerPage = 5
+)
+
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+	insert into articles (title, contents, username, nice, created_at) values
+	(?, ?, ?, 0, now());
+	`
+
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, _ := result.LastInsertId()
+	newArticle.ID = int(id)
+	return newArticle, nil
+}
+
+func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+		select *
+		from articles
+		where article_id = ?;
+	`
+
+	row := db.QueryRow(sqlStr, articleID)
+	var article models.Article
+	var createdTime sql.NullTime
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	return article, nil
+}
+
+func UpdateNiceNum(db *sql.DB, articleID int) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	const sqlUpdateNice = `update articles set nice = nice + 1 where article_id = ?`
+	_, err = tx.Exec(sqlUpdateNice, articleID)
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
